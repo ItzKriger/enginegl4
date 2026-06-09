792d1ed184de332778efb6dbd5f6beba60284a3f
ItzKriger <82546505+ItzKriger@users.noreply.github.com>
2026-06-09 13:43:33 +0300

    Create testscript.lua

---
+++ lua/main/testscript.lua
@@ -0,0 +1,3 @@
+Engine.Logger:Outln("Build info is "..Engine.BuildInfo.Date)
+Engine.Logger:Outln("Logger type is "..Engine.Logger:GetType())
+Engine.Logger:Outln("Player entity register result is "..tostring(__result_player))
